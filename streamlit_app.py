e28101498656e19c6effbbf3750062474032a14e
andtr-2021 <an.dtruongan@gmail.com>
2023-10-04 11:04:30 +0700

    almost done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import zipfile
+import os
+import shutil
+import os 
+import pandas as pd
+import numpy as np
+import cv2
+from sklearn.model_selection import train_test_split
 
 st.title("Automation for Data Preparation")
 
@@ -56,11 +64,6 @@ st.write("### Input Folder")
 st.write("Please update a zip file containing images in the default format.")
 input_zip_file = st.file_uploader("Upload a zip file", type=["zip"])
 
-# unzip the zip file
-import zipfile
-import os
-import shutil
-
 if st.button("Transform"):
     if input_zip_file is not None:
         with zipfile.ZipFile(input_zip_file, 'r') as zip_ref:
@@ -69,4 +72,50 @@ if st.button("Transform"):
 
 # store the default folder 
 default_folder = 'input_folder'
+X = []
+y = []
+
+counter = 0
+# check the folder structure to see if it is in the default format
+for folder in os.listdir(default_folder):
+    counter += 1
+    if counter == 2:
+        for data in os.listdir(folder):
+            curr_path = os.path.join(folder, data)
+            
+            for file in os.listdir(curr_path):
+                curr_file = os.path.join(curr_path, file)
+
+                if curr_file.endswith('.jpg'):
+                    
+                    img = cv2.imread(curr_file)
+                    img = cv2.resize(img, (224, 224))
+                    X.append(img)
+                    y.append(folder)
+
+
+                
+    
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+output_folder = 'output'
+
+for folder in np.unique(y):
+    curr_path = os.path.join(output_folder, 'train', folder)
+    os.makedirs(curr_path, exist_ok=True)
+
+    curr_path = os.path.join(output_folder, 'test', folder)
+    os.makedirs(curr_path, exist_ok=True)
+
+for i in range(len(X_train)):
+    curr_path = os.path.join(output_folder, 'train', y_train[i], str(i) + '.jpg')
+    cv2.imwrite(curr_path, X_train[i])
+
+for i in range(len(X_test)):
+    curr_path = os.path.join(output_folder, 'test', y_test[i], str(i) + '.jpg')
+    cv2.imwrite(curr_path, X_test[i])
+
+print('Done')
+   
 
